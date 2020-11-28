b24760a64bed5d821bedac8bb35e3ec6473d9c10
Kevin Lieu Git <klieu925@gmail.com>
2020-11-28 11:41:10 -0800

    week 2 workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -90,36 +90,6 @@ campsiteRouter
   });
 
 campsiteRouter
-  .route("/:campsiteId")
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    next();
-  })
-  .get((req, res) => {
-    res.end(
-      `Will send details of the campsite: ${req.params.campsiteId} to you`
-    );
-  })
-
-  .post((req, res) => {
-    res.statusCode = 403;
-    res.end(
-      `POST operation not supported on /campsites/${req.params.campsiteId}`
-    );
-  })
-
-  .put((req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name}
-          with description: ${req.body.description}`);
-  })
-
-  .delete((req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-  });
-
-campsiteRouter
   .route("/:campsiteId/comments")
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
