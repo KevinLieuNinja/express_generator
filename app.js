2ac19a11042e1d97706d8eac2a91ce6cd7c9b130
Kevin Lieu Git <klieu925@gmail.com>
2020-12-01 13:44:13 -0800

    sessions part 1

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+
 const mongoose = require("mongoose");
 const { runInNewContext } = require("vm");
 
@@ -35,11 +38,22 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+// app.use(cookieParser("12345-67890-09876-54321"));
+app,
+  use(
+    session({
+      name: "session-id",
+      secret: "12345-67890-09876-54321",
+      saveUninitialized: false,
+      resave: false,
+      store: new FileStore(),
+    })
+  );
 
 function auth(req, res, next) {
   console.log(req.headers);
-  if (!req.signedCookies.user) {
+
+  if (!req.session.user) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       const err = new Error("Your not authorized");
@@ -55,7 +69,7 @@ function auth(req, res, next) {
     const user = auth[0];
     const pass = auth[1];
     if (user === "admin" && pass === "password") {
-      res.cookie("user", "admin", { signed: true });
+      req.session.user = "admin";
       return next();
     } else {
       const err = new Error("Your not authorized");
@@ -64,7 +78,7 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "admin") {
       return next();
     } else {
       const err = new Error("Your not authorized!");
